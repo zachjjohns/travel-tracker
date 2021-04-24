7f4b55d98ab3806d4206b535e39d1655a2be0be0
Zach Johnson <zachjjohns@gmail.com>
2021-04-24 16:57:46 -0500

    Make html/css refactors to card display

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -11,10 +11,12 @@ let domUpdates = {
     trips.forEach(trip => {
       tripsContainer.innerHTML += 
       `<section class="card" id=${trip.id}>
-        <div class="destination-image" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${trip.image}); background-size: cover" alt="${trip.alt}">
+        <div class="destination-image" style=
+        "background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), 
+        url(${trip.image}); background-size: cover" alt="${trip.alt}">
           <div class="party-container">
-            <p class="party-title">Travelers</p>
-            <p class="party-count">${trip.travelers}</p>
+          <p class="party-title">Travelers</p>
+          <p class="party-count">${trip.travelers}</p>
           </div>
         </div>
         <div class="bottom-card-container">
