0139b6d493929d83ec7562fd9fe9c2b0167730d1
Zach Johnson <zachjjohns@gmail.com>
2021-04-24 21:54:23 -0500

    SCreate CSS files to sort

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import './css/main.scss';
-import {
-  getTraveler,
-  tripsData,
-  destinationsData
-} from './API';
+import {getTraveler, tripsData, destinationsData} from './API';
 import dayjs from 'dayjs';
 import Traveler from './Traveler.js'
 import domUpdates from './domUpdates';
-console.log(dayjs());
+
+const requestButton = document.querySelector('.request-trip');
+const tripsContainer = document.querySelector('.trips-container');
+const requestForm = document.querySelector('.request-form');
+
 let traveler, destinations
 
 window.addEventListener('load', getData(9));
+requestButton.addEventListener('click', displayRequestForm);
 
 function getData(id) {
   Promise.all([getTraveler(id), tripsData(), destinationsData()])
@@ -56,23 +57,33 @@ function getAnnualSpent() {
   domUpdates.displayAmountSpentThisYear(cost.toFixed(2));
 }
 
-function submitTripRequest() {
-   return fetch("http://localhost:3001/api/v1/trips", {
-      method: 'POST',
-      headers: {
-            'Content-Type': 'application/json'
-        },
-      body: JSON.stringify({
-        id: ,
-        userID: ,
-        destinationID: ,
-        travelers: ,
-        date: ,
-        duration: ,
-        status: "pending",
-        suggestedActivites: []
-      }))
-    .then(response => response.json())
-    .then(data => data)
-    .catch(err => console.log(`POST Error: ${err.message}`))
+function displayRequestForm() {
+  tripsContainer.classList.toggle("hidden");
+  requestForm.classList.toggle("hidden");
+  if (requestButton.innerText === "Request a Trip!") {
+    requestButton.innerText = "Return to Trips";
+  } else {
+    requestButton.innerText = "Request a Trip!";
+  }
 }
+
+// function submitTripRequest() {
+//    return fetch("http://localhost:3001/api/v1/trips", {
+//       method: 'POST',
+//       headers: {
+//             'Content-Type': 'application/json'
+//         },
+//       body: JSON.stringify({
+//         id: ,
+//         userID: ,
+//         destinationID: ,
+//         travelers: ,
+//         date: ,
+//         duration: ,
+//         status: "pending",
+//         suggestedActivites: []
+//       }))
+//     .then(response => response.json())
+//     .then(data => data)
+//     .catch(err => console.log(`POST Error: ${err.message}`))
+// }
