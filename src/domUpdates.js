5d95addaf29700aec2fbbd9e7f72aa0505aba607
Zach Johnson <zachjjohns@gmail.com>
2021-04-25 19:46:15 -0500

    Make accessibility and responsiveness changes

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -3,10 +3,11 @@ const tripsContainer = document.querySelector('#tripsContainer');
 const requestContainer = document.querySelector('#requestContainer');
 const destinationsDropdown = document.querySelector('#destinationsDropdown');
 const requestButton = document.querySelector('#requestTrip');
+const departDate = document.querySelector('#departDate');
 
 let domUpdates = {
   greetUser(traveler) {
-    const greeting = document.querySelector('.greeting');
+    const greeting = document.querySelector('#greeting');
     greeting.innerText = `Howdy, ${traveler.name}!`;
   },
 
@@ -16,7 +17,7 @@ let domUpdates = {
       tripsContainer.innerHTML += 
       `<section class="card" id=${trip.id}>
         <div class="destination-image" style=
-        "background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), 
+        "background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), 
         url(${trip.image}); background-size: cover" alt="${trip.alt}">
           <div class="party-container">
           <p class="party-title">Travelers</p>
@@ -43,6 +44,7 @@ let domUpdates = {
     travelerContainer.classList.toggle("hidden");
     tripsContainer.classList.toggle("hidden");
     requestContainer.classList.toggle("hidden");
+    this.setMinimumDate();
     if (requestButton.innerText === "Request a Trip!") {
       requestButton.innerText = "Return to Trips";
     } else {
@@ -55,9 +57,13 @@ let domUpdates = {
       destinationsDropdown.innerHTML += 
       `<option value="${dest.id}">${dest.destination}</option>`
     })
+  },
+
+  setMinimumDate() {
+    let today = new Date();
+    let minDate = today.toISOString().substring(0, 10);
+    departDate.setAttribute("min", minDate);
   }
-
-
 }
 
 export default domUpdates;
